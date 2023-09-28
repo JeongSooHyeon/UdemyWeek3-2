1d49d00ceea4226b7c612f3836bfe253de9bed9f
SooHyeon Jeong <jsh75721@naver.com>
2023-09-29 00:25:03 +0900

    주석 추가

--- Assets/1_Scripts/Rotator.cs
+++ Assets/1_Scripts/Rotator.cs
@@ -4,29 +4,28 @@ using UnityEngine;
 
 public class Rotator : MonoBehaviour
 {
-    public float rotationSpeed = 60f;
+    public float rotationSpeed = 60f;   // 회전 속력
 
-    private int rotationDir;
-    private float randInterval = 0f;
-
-    void Start()
-    {
-        
-    }
+    private int rotationDir;    // 회전 방향
+    private float randInterval = 0f;    // 회전 랜덤 주기
 
     void Update()
     {
         randInterval += Time.deltaTime;
-        if(randInterval >= 3)
+
+        if((int)randInterval >= 3)   // 3초 이상 지났으면
         {
-            randInterval = 0f;
-            rotationSpeed = Random.Range(30, 80);
-            rotationDir = Random.Range(0, 2);
-            if(rotationDir == 0)    // 25퍼센트 확률
+            randInterval = 0f;  // 랜덤 주기 초기화
+            rotationSpeed = Random.Range(30, 80);   // 랜덤으로 속력 정하기
+
+            // 50% 확률로 회전 방향 바꾸기
+            rotationDir = Random.Range(0, 2);  
+            if(rotationDir == 0)    
             {
                 rotationSpeed *= -1;
             }
         }
+
         transform.Rotate(0f, rotationSpeed*Time.deltaTime, 0f);
     }
 }
